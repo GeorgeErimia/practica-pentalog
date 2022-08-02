df5581290c4b0f6b2871bb23915c014c1268703f
GeorgeErimia <l3ttuceftw@gmail.com>
2022-08-02 17:18:53 +0300

    Created package "Tema1"; Created "ExercitiuRegex" class with main and exec.

---
+++ src/main/java/Tema1/ExercitiuRegex.java
@@ -0,0 +1,10 @@
+package Tema1;
+
+public class ExercitiuRegex {
+    public static void exec(){
+        System.out.println("-------------REGEX------------");
+    }
+    public static void main(String[] args) {
+        exec();
+    }
+}
