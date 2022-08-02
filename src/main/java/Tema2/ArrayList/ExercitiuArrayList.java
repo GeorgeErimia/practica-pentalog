6eee946ac7edf8709d5e845cd89ad3337d876099
GeorgeErimia <l3ttuceftw@gmail.com>
2022-08-02 17:34:06 +0300

    Created "printArray" method for "ExercitiuArrayList" class

--- src/main/java/Tema2/ArrayList/ExercitiuArrayList.java
+++ src/main/java/Tema2/ArrayList/ExercitiuArrayList.java
@@ -1,7 +1,19 @@
 package Tema2.ArrayList;
 
+import java.util.ArrayList;
+
 public class ExercitiuArrayList {
-    public static void exec(){
+
+    private static void printArray(ArrayList<String> list){
+        System.out.print("Lista are " + list.size() + " elemente: ");
+
+        // Parcurgem lista cu foreach
+        for(String str : list){
+            System.out.print(str + " ");
+        }
+        System.out.println("\r\n");
+    }
+    private static void exec(){
 
     }
     public static void main(String[] args) {
