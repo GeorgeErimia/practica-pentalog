2619abaf718ecd5e0bc2142c02ad13e7e2f4126a
GeorgeErimia <l3ttuceftw@gmail.com>
2022-08-02 17:29:29 +0300

    Added "printHashMap" method to ExercitiuHashMap

--- src/main/java/Tema2/HashMap/ExercitiuHashMap.java
+++ src/main/java/Tema2/HashMap/ExercitiuHashMap.java
@@ -1,8 +1,15 @@
 package Tema2.HashMap;
 
-public class ExercitiuHashMap {
-    public static void exec(){
+import java.util.HashMap;
 
+public class ExercitiuHashMap {
+    private static void printHashMap(HashMap<String, Integer> hashMap){
+        // Parcurgere dupa chei -> Se va observa ca jocurile nu se afla in ordinea in care au fost adaugate
+        for(String key : hashMap.keySet()){
+            System.out.println("joc: " + key +" | anul: " + hashMap.get(key));
+        }
+    }
+    public static void exec(){
     }
 
     public static void main(String[] args) {
