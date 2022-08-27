15d4e997e24a1559a64ce0ccad7838bd29bc40f0
GeorgeErimia <l3ttuceftw@gmail.com>
2022-08-27 11:59:43 +0300

    Initial Commit
    Added Spring Project to Git

---
+++ src/main/java/com/pentalog/project/ProiectPentalogPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.pentalog.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProiectPentalogPracticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProiectPentalogPracticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pentalog/project/ProiectPentalogPracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pentalog.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectPentalogPracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
